ff93bb1074e1e4c377e7ab7230671dc9905d9c0d
Guilherme Borlot Oliveira <guiborlot@edu.unifil.br>
2022-01-16 13:20:02 -0300

    Criando repository cliente, e o get /clientes retornando todos os clientes a partir do repositorio

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -1,31 +1,29 @@
 package com.algaworks.algalog.api.controller;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @RestController
 public class ClienteController {
-	
-	
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	public ClienteController(ClienteRepository clienteRepository) {
+		this.clienteRepository = clienteRepository;
+	}
+
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Joao");
-		cliente1.setTelefone("33 4444-4444");
-		cliente1.setEmail("jao@gmail.com");
-
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Maria");
-		cliente2.setTelefone("33 5555-4444");
-		cliente2.setEmail("maria@gmail.com");
-
-		return Arrays.asList(cliente1, cliente2);
+		return clienteRepository.findAll();
 	}
 
 
--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,15 +1,70 @@
 package com.algaworks.algalog.domain.model;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
+import java.util.Objects;
+
 @Getter
 @Setter
+@Entity
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome;
+
     private String email;
+
+    @Column(name = "fone")
     private String telefone;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cliente cliente = (Cliente) o;
+        return Objects.equals(id, cliente.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
